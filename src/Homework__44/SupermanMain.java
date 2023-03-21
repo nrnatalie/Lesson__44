585cd6513f7b23c1bec1f9042ec71abb1ee08cfe
Nathalie Russo <johndoe@example.com>
2023-03-21 15:32:45 +0200

    Add  implements Superman

--- src/Homework__44/SupermanMain.java
+++ src/Homework__44/SupermanMain.java
@@ -3,5 +3,30 @@ package Homework__44;
    // Напишите интерфейсы "Умеет летать" CanFly с методом fly(), "Умеет бегать"
    // CanRun с методом run и "умеет плавать"
    // CanSwim с методом swim и реализуйте их в классе "Супермен" Superman.
-public class SupermanMain {
+public class SupermanMain implements CanFly, CanRun, CanSwim {
+
+  @Override
+  public int getFly() {
+    return 0;
+  }
+
+  @Override
+  public int getRun() {
+    return 0;
+  }
+
+  @Override
+  public int getSwim() {
+    return 0;
+  }
+
+  @Override
+  public void faster(int difference) {
+
+  }
+
+  @Override
+  public void slower(int difference) {
+
+  }
 }
