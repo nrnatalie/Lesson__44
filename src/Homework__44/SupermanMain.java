c754e962671829734f65e39f12e0fb2108bf468b
Nathalie Russo <johndoe@example.com>
2023-03-21 15:15:21 +0200

    Add class Superman

---
+++ src/Homework__44/SupermanMain.java
@@ -0,0 +1,7 @@
+package Homework__44;
+//Задача 1
+   // Напишите интерфейсы "Умеет летать" CanFly с методом fly(), "Умеет бегать"
+   // CanRun с методом run и "умеет плавать"
+   // CanSwim с методом swim и реализуйте их в классе "Супермен" Superman.
+public class SupermanMain {
+}
